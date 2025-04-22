d1953333da131267f84a30dd7e1e302c30e22412
heronhoga <nozzeravixer@gmail.com>
2025-04-22 20:34:01 +0700

    fix:fix routes middleware to use api key

--- routes/route_memos.go
+++ routes/route_memos.go
@@ -9,8 +9,8 @@ import (
 
 func MemoRoutes(h *http.ServeMux) {
 	//protected routes
-	h.Handle("POST /memo/create", utils.CheckToken((http.HandlerFunc(handlers.CreateMemo))))
-	h.Handle("GET /memo", utils.CheckToken((http.HandlerFunc(handlers.ReadMemo))))
-	h.Handle("PUT /memo", utils.CheckToken((http.HandlerFunc(handlers.UpdateMemo))))
-	h.Handle("DELETE /memo", utils.CheckToken((http.HandlerFunc(handlers.DeleteMemo))))
+	h.Handle("POST /memo/create", utils.CheckKey(utils.CheckToken((http.HandlerFunc(handlers.CreateMemo)))))
+	h.Handle("GET /memo", utils.CheckKey(utils.CheckToken((http.HandlerFunc(handlers.ReadMemo)))))
+	h.Handle("PUT /memo", utils.CheckKey(utils.CheckToken((http.HandlerFunc(handlers.UpdateMemo)))))
+	h.Handle("DELETE /memo", utils.CheckKey(utils.CheckToken((http.HandlerFunc(handlers.DeleteMemo)))))
 }
--- routes/route_users.go
+++ routes/route_users.go
@@ -9,9 +9,9 @@ import (
 
 func UserRoutes(h *http.ServeMux) {
 	// Public routes
-	h.Handle("POST /register", http.HandlerFunc(handlers.Register))
-	h.Handle("POST /login", http.HandlerFunc(handlers.Login))
+	h.Handle("POST /register", utils.CheckKey(http.HandlerFunc(handlers.Register)))
+	h.Handle("POST /login", utils.CheckKey(http.HandlerFunc(handlers.Login)))
 
 	// Protected route
-	h.Handle("GET /logout", utils.CheckToken((http.HandlerFunc(handlers.Logout))))
+	h.Handle("GET /logout", utils.CheckKey(utils.CheckToken((http.HandlerFunc(handlers.Logout)))))
 }
